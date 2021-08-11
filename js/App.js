417a1d011c5d09b4a24b18b4a5d92b82a5225dfd
Yousef Allam <yousef.allam221b@gmail.com>
2021-08-11 20:33:48 +0200

    working projects buttons and some few updates

--- js/App.js
+++ js/App.js
@@ -48,3 +48,94 @@ for (element of phoneElements)
 {
   element.addEventListener('click', function () {window.location.href = 'tel://+201001097153';})
 }
+
+/*
+var but = document.getElementById('but');
+but.addEventListener('click', function () {
+var divWidth = document.getElementById('row-container').clientWidth;
+var cardLeftWidth = document.getElementsByClassName('show')[0].offsetLeft;
+
+var collapseItem = document.getElementsByClassName('collapsing')[0];
+collapseItem.style.width = divWidth - cardLeftWidth;
+console.log(collapseItem.clientWidth);
+console.log(divWidth - cardLeftWidth);
+console.log(collapseItem.clientWidth);});
+*/
+
+
+
+/*
+var seeMore = document.getElementById('see-more-button');
+
+seeMore.addEventListener('click', function (){
+
+  setTimeout(function () {
+    var divWidth = document.getElementById('row-container').clientWidth;
+    var cardLeftWidth = document.getElementsByClassName('collapsing')[0].offsetLeft;
+
+    var collapseItem = document.getElementsByClassName('collapsing')[0];
+    var x = divWidth - cardLeftWidth*10;
+    collapseItem.style.width = ''+ x +'px';
+
+
+  }, 1);
+
+  setTimeout(function () {
+    var divWidth = document.getElementById('row-container').clientWidth;
+    var cardLeftWidth = document.getElementsByClassName('show')[0].offsetLeft;
+
+    var collapseItem = document.getElementById('help');
+    var x = divWidth - cardLeftWidth*10;
+    collapseItem.style.width = ''+ x +'px';
+
+
+  },1000);
+
+});
+
+var el = document.getElementById('card-container');
+console.log(el);
+
+//const {top, left, width, height} = element.getBoundingClientRect();
+
+
+*/
+/*
+
+var seeMore = document.getElementById('see-more-button');
+seeMore.addEventListener('click', function (){
+
+  setTimeout(function () {
+    var divWidth = document.getElementById('row-container').clientWidth;
+    var cardLeftWidth = document.getElementsByClassName('collapsing')[0].offsetLeft;
+
+    var collapseItem = document.getElementsByClassName('collapsing')[0];
+    var x = divWidth - cardLeftWidth*11;
+    collapseItem.style.width = ''+ x +'px';
+    console.log(x);
+
+  }, 1);
+});
+
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//
